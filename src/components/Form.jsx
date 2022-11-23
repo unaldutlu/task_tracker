74d162b59374baafd6cea3dc6fe8e557b6f1706e
unaldutlu <unaldutlu@gmail.com>
2022-11-23 15:11:25 +0300

    add css-responsive on page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -59,8 +59,8 @@ function Form({ addForm, setAddForm, isClick }) {
               onChange={onChangeInput}
             />
           </div>
-          <div className='text-center mt-3'>
-            <button className='py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 w-20'>
+          <div className='flex justify-center mt-3'>
+            <button className='py-2 px-4 bg-blue-500 flex justify-center text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 w-[15%]'>
               Add
             </button>
           </div>
